9f2e3bbc22f7973aac71ecad9e949ecca6bafb10
zZHugoZz <estruchhugo@gmail.com>
2023-07-27 15:22:31 +0200

    added the delete functions
    added the delete function in the workout_exercises and program_days routes

--- app/routers/program_days.py
+++ app/routers/program_days.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Security, status
+from fastapi import APIRouter, Depends, HTTPException, Response, Security, status
 from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -21,13 +21,38 @@ def create_program_day(
     db: Session = Depends(get_db),
 ):
     user_id = decode_token(credentials.credentials)
-    program_id = db.query(models.Program).filter(
+    program_query = db.query(models.Program).filter(
         models.Program.id == program_day.program_id
     )
-    if program_id.first().user_id != user_id:
+    if program_query.first().user_id != user_id:
         raise FORBIDDEN_EXCEPTION
     created_program_day = models.ProgramDay(**program_day.model_dump())
     db.add(created_program_day)
     db.commit()
     db.refresh(created_program_day)
     return created_program_day
+
+
+@router.delete("/{program_day_id}", status_code=status.HTTP_200_OK)
+def delete_program_day(
+    program_day_id: int,
+    credentials: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    user_id = decode_token(credentials.credentials)
+    program_day_query = db.query(models.ProgramDay).filter(
+        models.ProgramDay.id == program_day_id
+    )
+    if program_day_query.first() is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Program day with id: {program_day_id} doesn't exist",
+        )
+    program_query = db.query(models.Program).filter(
+        models.Program.id == program_day_query.first().program_id
+    )
+    if program_query.first().user_id != user_id:
+        raise FORBIDDEN_EXCEPTION
+    program_day_query.delete()
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/routers/workout_exercises.py
+++ app/routers/workout_exercises.py
@@ -21,13 +21,37 @@ def create_workout_exercise(
     db: Session = Depends(get_db),
 ):
     user_id = decode_token(credentials.credentials)
-    workout_id = db.query(models.Workout).filter(
+    workout_query = db.query(models.Workout).filter(
         models.Workout.id == exercise.workout_id
     )
-    if workout_id.first().user_id != user_id:
+    if workout_query.first().user_id != user_id:
         raise FORBIDDEN_EXCEPTION
     created_exercise = models.WorkoutExercise(**exercise.model_dump())
     db.add(created_exercise)
     db.commit()
     db.refresh(created_exercise)
     return created_exercise
+
+
+@router.delete("/{workout_id}", status_code=status.HTTP_200_OK)
+def delete_workout_exercise(
+    workout_exercise_id: int,
+    credentials: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    user_id = decode_token(credentials.credentials)
+    workout_exercise_query = db.query(models.WorkoutExercise).filter(
+        models.WorkoutExercise.id == workout_exercise_id
+    )
+    if workout_exercise_query.first() is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Exercise with id: {workout_exercise_id} doesn't exist",
+        )
+    workout_query = db.query(models.Workout).filter(
+        models.Workout.id == workout_exercise_query.first().workout_id
+    )
+    if workout_query.first().user_id != user_id:
+        return FORBIDDEN_EXCEPTION
+    workout_exercise_query.delete()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
