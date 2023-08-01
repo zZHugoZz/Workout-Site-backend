755e55fd304ca8de038fadfd30f45819d7f47222
zZHugoZz <estruchhugo@gmail.com>
2023-08-01 12:05:29 +0200

    added the create endpoint in the performance route

--- app/routers/performances.py
+++ app/routers/performances.py
@@ -1,4 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, Security
+from fastapi.security import HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+from app.database import get_db
+from ..schemas import Performance, PerformanceIn
+from .authentication import security
+from ..utils import create
+from .. import models
 
 
 router = APIRouter(prefix="/performances", tags=["Performances"])
+
+
+@router.post("/", status_code=status.HTTP_200_OK, response_model=Performance)
+def create_performance(
+    performance: PerformanceIn,
+    credentials: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    return create(credentials, db, models.Performance, performance)
