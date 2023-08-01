72dd2ff05d1f7cd95d6d8d3c1421397c6b7c68ae
zZHugoZz <estruchhugo@gmail.com>
2023-08-01 18:17:01 +0200

    added the get_items and get_item functions to the workout route

--- app/routers/workouts.py
+++ app/routers/workouts.py
@@ -1,12 +1,10 @@
-from fastapi import Depends, status, APIRouter, Response, Security
+from fastapi import Depends, status, APIRouter, Security
 from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from .. import models
 from ..database import get_db
 from .. import schemas
 from .authentication import security
-from ..oauth2 import decode_token
-from ..utils import FORBIDDEN_EXCEPTION, NOT_FOUND_EXCEPTION
 from datetime import date
 from ..utils import create, delete, get_items, get_item
 
@@ -19,9 +17,6 @@ def get_workouts(
     credentials: HTTPAuthorizationCredentials = Security(security),
     db: Session = Depends(get_db),
 ):
-    # user_id = decode_token(credentials.credentials)
-    # workouts = db.query(models.Workout).filter(models.Workout.user_id == user_id).all()
-    # return workouts
     return get_items(credentials, db, models.Workout)
 
 
@@ -31,13 +26,7 @@ def get_workout(
     credentials: HTTPAuthorizationCredentials = Security(security),
     db: Session = Depends(get_db),
 ):
-    user_id = decode_token(credentials.credentials)
-    workout_query = db.query(models.Workout).filter(models.Workout.id == id)
-    if workout_query.first() is None:
-        raise NOT_FOUND_EXCEPTION("workout", id)
-    if workout_query.first().user_id != user_id:
-        raise FORBIDDEN_EXCEPTION
-    return workout_query.first()
+    return get_item(id, credentials, db, models.Workout, "Workout")
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Workout)
