55c3d392295428a29f47092ae87b11e046d669bd
zZHugoZz <estruchhugo@gmail.com>
2023-08-02 08:31:57 +0200

    added the get_unit endpoint to the units route

--- app/routers/units.py
+++ app/routers/units.py
@@ -4,11 +4,20 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from ..schemas import Unit, UnitIn
 from .authentication import security
-from ..utils import update
+from ..utils import update, get_item
 from .. import models
 
 
 router = APIRouter(prefix="/units", tags=["Units"])
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=Unit)
+def get_unit(
+    id: int,
+    credentials: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    return get_item(id, credentials, db, models.Unit, "Unit")
 
 
 @router.put("/{id}", status_code=status.HTTP_200_OK, response_model=Unit)
