d6729c495dd3e7ab272261469d44ee70a5da1c60
zZHugoZz <estruchhugo@gmail.com>
2023-08-04 18:51:51 +0200

    added the get_bodyweights endpoint

--- app/routers/bodyweights.py
+++ app/routers/bodyweights.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from .authentication import security
 from ..schemas import BodyWeight, BodyWeightIn
-from ..utils import create
+from ..utils import create, get_items
 from .. import models
 from datetime import date
 
@@ -20,3 +20,11 @@ def create_bodyweight(
 ):
     current_date = {"date": date.today()}
     return create(credentials, db, models.BodyWeight, bodyweight, current_date)
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[BodyWeight])
+def get_bodyweights(
+    credentials: HTTPAuthorizationCredentials = Security(security),
+    db: Session = Depends(get_db),
+):
+    return get_items(credentials, db, models.BodyWeight)
