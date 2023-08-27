308953b3b7b2d7c9e2293d10cfff46b2dd46799d
zZHugoZz <estruchhugo@gmail.com>
2023-08-27 16:34:34 +0200

    finished the logic to send the profile pic

--- app/routers/profile_pictures.py
+++ app/routers/profile_pictures.py
@@ -1,5 +1,6 @@
 import io
 from fastapi import APIRouter, status, UploadFile
+from fastapi.responses import FileResponse, StreamingResponse
 from google.oauth2 import service_account
 from googleapiclient.discovery import build, Resource
 from googleapiclient.http import MediaIoBaseUpload
@@ -13,11 +14,11 @@ router = APIRouter(prefix="/profile_pictures", tags=["Profile pictures"])
 
 credentials_path = config.settings.google_json_credentials_path
 credentials = service_account.Credentials.from_service_account_file(credentials_path)
+drive_service: Resource = build("drive", "v3", credentials=credentials)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def upload_profile_picture(file_in: UploadFile, params: common_deps):
-    drive_service: Resource = build("drive", "v3", credentials=credentials)
     file_metadata = {"name": file_in.filename}
     file_content = await file_in.read()
 
@@ -35,9 +36,15 @@ async def upload_profile_picture(file_in: UploadFile, params: common_deps):
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_profile_picture(params: common_deps):
-    profile_picture = await generic_operations.get_item(
-        params[Dependencies.CREDENTIALS],
-        params[Dependencies.DB],
-        profile_pictures_model.ProfilePicture,
-        "Profile picture",
+    print("credentials: ", params[Dependencies.CREDENTIALS])
+    profile_picture: profile_pictures_model.ProfilePicture = (
+        await generic_operations.get_item(
+            params[Dependencies.CREDENTIALS],
+            params[Dependencies.DB],
+            profile_pictures_model.ProfilePicture,
+            "Profile picture",
+        )
     )
+    file_id = profile_picture.picture_id
+    file_content = drive_service.files().get_media(fileId=file_id).execute()
+    return StreamingResponse(io.BytesIO(file_content), media_type="image/jpeg")
